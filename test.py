0bedc64cf08fc31e86da65ba8fe467ac32e35e4f
Will Buesnel <BuesnelWill@gmail.com>
2024-04-13 13:24:50 +0100

    Added functionality for the graphs

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import random
-
+import numpy as np
 class Person:
     def __init__(self, opinion,position):
         self.opinion = opinion
@@ -26,7 +26,7 @@ def apply_rule(person: Person, neighbour: Person, T, beta):
 def decide_rand_neighbour(person : Person, _grid : list):
     # if at left side of grid, choose right neighbour
     if person.position == 0:
-        random_neighbour_index = 2 
+        random_neighbour_index = 2
     # if at right side of grid, choose left neighbour
     elif person.position == len(_grid)-1:
         random_neighbour_index = len(_grid)-2
@@ -41,19 +41,54 @@ def update_grid(_grid, T, beta):
     random_neighbour = decide_rand_neighbour(random_person,_grid)
     # apply rule to them.
     apply_rule(random_person,random_neighbour,T,beta)
-    pass
+    return _grid
 
 def plot_timestep(timestep):
     # plot the grid at the given timestep
     pass
 
-def main(T=0.2, beta=0.2,size=10):
+
+def get_scatter_values(float_list: list):
+    y_values = []
+    x_values = []
+
+    for x in range(0,len(float_list)-1):
+        x_values.append([x] * (len(float_list[x])))
+        for y in float_list[x]:
+            y_values.append(y)
+    return x_values,y_values
+
+def plot_hist(ax, grid):
+    final_opinions = [grid[i].opinion for i in range(0,len(grid)-1)]
+    ax.hist(final_opinions)
+    ax.set_xlim([0, 1])
+    ax.set_xlabel("Opinion")
+
+
+def plot_scatter(ax, opinon_list):
+    timestep_values, opinion_values = get_scatter_values(opinon_list)
+    ax.scatter(timestep_values,opinion_values, color ="red") 
+    ax.set_ylim([0,1])
+    ax.set_ylabel("Opinion")
+    ax.set_xlabel("Timestep")
+
+def main(T=0.2, beta=0.2,size=20):
     # generate grid
     grid = generate_1d_grid(size)
+    opinions_list = [[grid[i].opinion for i in range(0, len(grid) - 1)]]
     for timestep in range(100):
-        update_grid(grid,T,beta)
+        grid = update_grid(grid,T,beta)
+        opinions_list.append([grid[i].opinion for i in range(0,len(grid)-1)])
+        # 2d list of floats, each nthlist contains the opinions for timestep n.
     # update grid for 100 time steps
-    pass
+        
+
+    fig, axs = plt.subplots(1,2)
+    plot_hist(axs[0],grid)
+    plot_scatter(axs[1],opinions_list)
+    title = str("Threshold = "  +str(T) +", beta = " +str(beta))
+    fig.suptitle(title)
+    plt.show()
 
 
 # run main function
